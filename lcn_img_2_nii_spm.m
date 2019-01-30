f0eaffe64a59768c0f944f70918f589e97d142cb
eshwargg <40626534+eshwargg@users.noreply.github.com>
2019-01-30 17:08:22 +0100

    Add files via upload

---
+++ lcn_img_2_nii_spm.m
@@ -0,0 +1,18 @@
+
+
+input = 's04_anatomical_iso.img';
+[pathstr,fname,ext] = fileparts(input);
+if strcmp(pathstr,'')
+   pathstr = pwd; 
+end
+
+output = strcat(fname,'.nii');
+V=spm_vol(input);
+ima=spm_read_vols(V);
+V.fname=output;
+
+% save 
+currentFolder = pwd;
+cd(pathstr);
+spm_write_vol(V,ima);
+cd(currentFolder);
